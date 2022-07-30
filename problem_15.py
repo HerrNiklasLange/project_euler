837524356a472558499cd1c58ac86ce054e8a798
HeerNiklasLange <niklas62.36@gmail.com>
2022-07-30 23:56:08 +0100

    as

---
+++ problem_15.py
@@ -0,0 +1,2 @@
+# I was all day out and I forgot to programm so I am just doing this, studying for my SAT and then going to bed
+# I need to get back to daily programming but right now I am failling 
