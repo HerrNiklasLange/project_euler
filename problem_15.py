07c58706c2c4754bf221bf07eebfb0d90f7a93ea
HeerNiklasLange <niklas62.36@gmail.com>
2022-07-31 09:18:36 +0100

    working on problem 15

--- problem_15.py
+++ problem_15.py
@@ -1,2 +1,39 @@
 # I was all day out and I forgot to programm so I am just doing this, studying for my SAT and then going to bed
 # I need to get back to daily programming but right now I am failling 
+def create_grid(n):
+    grid = []
+    for y in range(0,n + 1):
+        y =  y 
+        for x in range(0, n +1):
+            x = x * 10 + y
+            grid.append(x)
+    return grid
+
+
+def main(n):
+    grid = create_grid(n)
+    print(grid)
+    x = 10
+    y = 1
+    path = 0
+    # the goal is to find all the ways to grid 22
+    # we can move + 10 (x) or + 1 (y)
+    for row1 in range(0, n +1):
+        holder = 0
+        holder += row1 * x
+        holder += y
+        print(holder)
+        if not holder > 22:
+            for row2 in range(0, n +1):
+                holder += row2 * x
+                holder += y
+                print(holder)
+                if not holder > 22:
+                    for row3 in range(0, n +1):
+                        holder += row3 * x
+                        holder += y
+                        print(holder)
+                        if holder == 22:
+                            path += 1
+    print(path)
+main(2)
