7617cab48a63a8306644e297cde968f21c483502
HeerNiklasLange <niklas62.36@gmail.com>
2022-08-02 23:45:19 +0100

    aa

--- problem_15.py
+++ problem_15.py
@@ -1,5 +1,4 @@
-# I was all day out and I forgot to programm so I am just doing this, studying for my SAT and then going to bed
-# I need to get back to daily programming but right now I am failling 
+
 def create_grid(n):
     grid = []
     for y in range(0,n + 1):
